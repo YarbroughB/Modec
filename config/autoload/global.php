a3fabc770768dad166590b123602bd9f4052b5c1
Brian Yarbrough <bay6g7@mst.edu>
2016-03-06 05:33:33 -0600

    Fixing database issue with server

--- config/autoload/global.php
+++ config/autoload/global.php
@@ -13,10 +13,7 @@
 
 return array(
 	'db' => array(
-		'driver' => 'Pdo',
-		'driver_options' => array(
-			PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES \'UTF8\''
-		),
+		'driver' => 'Pdo_Mysql',
 	),
 	'service_manager' => array(
 		'factories' => array(
